bb37532da8d009116723d7b216e3f9ed44de8de5
Lebowski <lebowski@LebowskideMacBook-Pro.local>
2022-10-22 14:43:21 +0800

    practise 7-03 Returning_functions

--- Work/stock.py
+++ Work/stock.py
@@ -1,21 +1,17 @@
+from typedproperty import typedproperty, String, Integer, Float
+
 #
 class Stock:
-    __slots__ = ('name', '_shares', 'price')
+    # __slots__ = ('name', '_shares', 'price')
+
+    name = String('name')
+    shares = Integer('shares')
+    price = Float('price')
 
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
         self.price = price
-
-    @property
-    def shares(self):
-        return self._shares
-
-    @shares.setter
-    def shares(self, value):
-        if not isinstance(value, int):
-            raise TypeError('Expected int')
-        self._shares = value
 
     @property
     def cost(self):
